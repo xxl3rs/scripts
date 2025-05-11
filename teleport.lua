0c8745bcd328d03474d74a73d4a1eedea391e94e
xl3s <145204861+xxl3rs@users.noreply.github.com>
2025-05-11 20:17:21 +0300

    update

--- teleport.lua
+++ teleport.lua
@@ -1,4 +1,4 @@
-local repo = 'https://raw.githubusercontent.com/xxl3rs/mainlib/main'
+local repo = 'https://raw.githubusercontent.com/xxl3rs/mainlib/main/'
 
 local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
 local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
