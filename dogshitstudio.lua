7570297aec23ef2fe99f43ba51a6b6638631385c
xl3s <145204861+xxl3rs@users.noreply.github.com>
2025-06-18 19:17:35 +0300

    going to stay unchanged 💔💔

---
+++ dogshitstudio.lua
@@ -0,0 +1,4 @@
+require(game:GetService("ReplicatedStorage")._RetroStudio.ScriptService.Executors.Util).isStudio = function()
+    return true
+end
+queueonteleport(game:HttpGet("https://raw.githubusercontent.com/xxl3rs/scripts/refs/heads/main/dogshitstudio.lua"))
