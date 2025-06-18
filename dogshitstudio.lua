398339edc7db68452182a7b6c9143bc2876e310c
xl3s <145204861+xxl3rs@users.noreply.github.com>
2025-06-18 19:19:01 +0300

    or maybe there will

--- dogshitstudio.lua
+++ dogshitstudio.lua
@@ -1,4 +1,5 @@
+if game:GetService("ReplicatedStorage"):FindFirstChild("_RetroStudio") then
 require(game:GetService("ReplicatedStorage")._RetroStudio.ScriptService.Executors.Util).isStudio = function()
     return true
-end
+end end
 queueonteleport(game:HttpGet("https://raw.githubusercontent.com/xxl3rs/scripts/refs/heads/main/dogshitstudio.lua"))
