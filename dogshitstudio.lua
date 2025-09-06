3f9cb41c73292222b53cc2bf3c4155b4954ac8ec
xl3s <145204861+xxl3rs@users.noreply.github.com>
2025-09-06 16:07:05 +0300

    Delete dogshitstudio.lua

--- dogshitstudio.lua
+++
@@ -1,5 +0,0 @@
-if game:GetService("ReplicatedStorage"):FindFirstChild("_RetroStudio") then
-require(game:GetService("ReplicatedStorage")._RetroStudio.ScriptService.Executors.Util).isStudio = function()
-    return true
-end end
-queueonteleport(game:HttpGet("https://raw.githubusercontent.com/xxl3rs/scripts/refs/heads/main/dogshitstudio.lua"))
